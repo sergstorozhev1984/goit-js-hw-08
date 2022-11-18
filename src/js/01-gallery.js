592027f2d6457946e8a6095c8c8e9d61fd169cd7
Sergey <92057970+sergstorozhev1984@users.noreply.github.com>
2022-11-18 11:42:42 +0200

    hw 8 1&2 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,19 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm";
 import { galleryItems } from './gallery-items';
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
 console.log(galleryItems);
+
+const refs = {
+    galleryEl: document.querySelector('.gallery'),
+}
+
+function createGalleryListElMarcup(galleryItems) {
+    let galleryListEl = galleryItems.map(el => `<li><a class="gallery__item" href="${el.original}"><img class="gallery__image" src="${el.preview}" alt="${el.description}"></a></li>`).join('');
+    refs.galleryEl.innerHTML = galleryListEl;
+}
+createGalleryListElMarcup(galleryItems);
+
+let lightbox = new SimpleLightbox('.gallery a', { captionsData: "alt", captionDelay: 250 });
