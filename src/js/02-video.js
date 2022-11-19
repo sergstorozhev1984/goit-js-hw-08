b95c9e6ad084db97e78237143008ae0792dbd2f6
Sergey <92057970+sergstorozhev1984@users.noreply.github.com>
2022-11-19 04:04:23 +0200

    3 tasks done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,15 +1,16 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
+import {saveToLS, loadFromLS}  from './helpers';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
  
 player.on('timeupdate', throttle(e => {
-    localStorage.setItem('videoplayer-current-time', e.seconds);
+    saveToLS('videoplayer-current-time', e.seconds);
 }, 1000));
 
 player
-  .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+  .setCurrentTime(loadFromLS('videoplayer-current-time'))
   .catch(function (error) {
     switch (error.name) {
       case 'RangeError':
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import {saveToLS, loadFromLS} from './helpers';
+import throttle from 'lodash.throttle';
+
+
+const refs = {
+    formEl: document.querySelector('.feedback-form'),
+}
+function onLoad() {
+    const emailValue = loadFromLS('feedback-form-state').email;
+    refs.formEl.elements.email.value = emailValue || '';
+    const messageValue = loadFromLS('feedback-form-state').message
+    refs.formEl.elements.message.value = messageValue || '';
+}
+onLoad();
+refs.formEl.addEventListener('submit', onFormSubmit);
+function onFormSubmit (e) {
+    e.preventDefault();
+    localStorage.clear();
+
+    const formData = new FormData(e.target);
+    const objData = {};
+
+    for (let [key, value] of formData) {
+        objData[key] = value;
+    }
+
+    console.log(objData);
+    e.target.reset();
+}
+
+
+refs.formEl.addEventListener('input', throttle(e => {
+    const obj = {};
+    obj.email = refs.formEl.elements.email.value;
+    obj.message = refs.formEl.elements.message.value;
+    saveToLS('feedback-form-state', obj);
+}, 500));
 
